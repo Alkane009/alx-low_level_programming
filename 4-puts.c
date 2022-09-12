7284ad2147ac58d9bcc2b4d005cbb5106ccf0c0f
Alkane009 <akewusholamuktar1@gmail.com>
2022-09-12 16:05:47 -0700

    Hello_world

---
+++ 4-puts.c
@@ -0,0 +1,64 @@
+/**
+
+*main - print the sting in the function
+*
+*Description: using the main function
+*this program prints "programming is like building a multilingual puzzle 
+*Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
+c
+^c
+^C
+
+clear
+^c
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
